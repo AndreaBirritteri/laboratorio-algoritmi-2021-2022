252c4045fb5a584bd9476d6e35a350f07ad519c7
Raoul <raoul.rondinella@edu.unito.it>
2022-06-27 15:23:55 +0200

    Creato makefile

--- Resources/pippo.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by U1 on 27/06/2022.
-//
-
-#ifndef LABORATORIO_ALGORITMI_2021_2022_PIPPO_H
-#define LABORATORIO_ALGORITMI_2021_2022_PIPPO_H
-
-#endif //LABORATORIO_ALGORITMI_2021_2022_PIPPO_H
-hjcdbsjalfbgaldbghjldfjbadbubfhbwaehbfuewbhfbuawehfbwaehfbuawhufbabfhwebufhwabefhaw
